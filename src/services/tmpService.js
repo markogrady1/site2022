aebb9ee357a390b79d33a61d395d4d1fa232e1cd
Mark O Grady <mark.ogrady@broadbean.com>
2022-01-12 01:00:25 +0000

    fix typo

--- src/services/tmpService.js
+++ src/services/tmpService.js
@@ -1,12 +1,12 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-export async function getProducts(category) {
+export async function getProjects(category) {
   const response = await fetch(baseUrl + 'projects=' + category);
   if (response.ok) return response.json();
   throw response;
 }
 
-export async function getProduct(id) {
+export async function getProject(id) {
   const response = await fetch(baseUrl + 'projects/' + id);
   if (response.ok) return response.json();
   throw response;

88065b45c091939b428603d40fc554d7beee4f5e
Mark O Grady <mark.ogrady@broadbean.com>
2022-01-14 23:06:55 +0000

    add github page styling

--- src/components/GitHubRepos.js
+++ src/components/GitHubRepos.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import useFetch from '../services/useFetch';
-
 import Spinner from './Spinner';
 import PageNotFound from './PageNotFound';
 
@@ -16,9 +14,7 @@ function GitHubRepos() {
   );
 
   if (loading) return <Spinner />;
-
   if (!projects) return <PageNotFound />;
-
   if (error) throw error;
 
   const usefulProjects = [
@@ -33,9 +29,15 @@ function GitHubRepos() {
     'site2022',
   ];
 
-  function retrieveUseful(items) {
+  const retrieveUseful = (items) => {
     return usefulProjects.find((item) => item === items.name);
-  }
+  };
+
+  const notNpmPublished = (repo) => {
+    const notNpm = ['jQit', 'widgetJS', 'site2022', 'design-patterns'];
+
+    return !notNpm.includes(repo);
+  };
 
   const filteredProjects = projects.filter(retrieveUseful);
 
@@ -44,8 +46,29 @@ function GitHubRepos() {
       <div className='title'>GitHub Repositories</div>
       <div className=''>
         {filteredProjects.map((item) => (
-          <a key={item.id} href={item.html_url}>
-            <p>{item.name}</p>
+          <a
+            className='github-anchor'
+            target='_blank'
+            rel='noopener noreferrer'
+            href={item.html_url}
+          >
+            <div key={item.id} className='github-container'>
+              <p className='github-name'>{item.full_name}</p>
+              <p className='github-desc'> {item.description}</p>
+              {notNpmPublished(item.name) && (
+                <div>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href={`https://www.npmjs.com/package/${item.name}`}
+                  >
+                    <img
+                      src={`https://img.shields.io/npm/v/${item.name}.svg`}
+                    ></img>
+                  </a>
+                </div>
+              )}
+            </div>
           </a>
         ))}
       </div>

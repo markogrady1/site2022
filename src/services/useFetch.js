0cbbb7e3a57de7a741f7d3f73d88385c1f9aafdf
Mark O Grady <mark.ogrady@broadbean.com>
2022-03-06 22:48:13 +0000

    add potential docker usage

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,4 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
+// temporary solution
+import response  from './../utils/db.json';
 
 export default function useFetch(url, base = null) {
   const isMounted = useRef(false);
@@ -10,15 +12,34 @@ export default function useFetch(url, base = null) {
     isMounted.current = true;
     async function init() {
       try {
-        let baseUrl = base ?? process.env.REACT_APP_API_BASE_URL;
-
-        const response = await fetch(baseUrl + url);
-        if (response.ok) {
-          const json = await response.json();
-          if (isMounted.current) setData(json);
+// ---- REMOVE FROM HERE
+        if (base) {
+          const res = await fetch(base + url);
+          if (res.ok) {
+            const json = await res.json();
+            if (isMounted.current) setData(json);
         } else {
-          throw response;
+          throw res;
         }
+        } else {
+        // faking it until api hooked up
+          if (response) {
+            setData(response[url]);
+          } else {
+            throw response;
+          }
+        }
+// ---- TO HERE --- AND UNCOMMENT BELOW
+
+      //  let baseUrl = base ?? process.env.REACT_APP_API_BASE_URL;
+
+      //  const response = await fetch(baseUrl + url);
+      //  if (response.ok) {
+      //    const json = await response.json();
+      //    if (isMounted.current) setData(json);
+      //    } else {
+      //      throw response;
+      //    }
       } catch (e) {
         if (isMounted.current) setError(e);
       } finally {
